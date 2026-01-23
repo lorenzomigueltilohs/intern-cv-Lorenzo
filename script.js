4c87cd7715c430f04d9b5f78fc48765fd91c5b3b
Lorenzo Miguel Tilo <lorenzomigueltilo.hs@gmail.com>
2026-01-23 11:09:58 +0800

    Toggle Dark Mode and Welcome Message Using JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const userName = document.querySelector("header h1").textContent;
-  alert("Welcome to my website, " + userName + "!");
+
+  const modal = document.getElementById("welcomeModal");
+  const welcomeText = document.getElementById("welcomeText");
+  const closeButton = document.getElementById("closeModal");
+
+  welcomeText.textContent = "Welcome to my website take a look around and learn more about my skills, education and experiences" + "!";
+  modal.showModal();
+
+  closeButton.addEventListener("click", function () {
+    modal.close();
+  });
 
    // Dark Mode
   const toggleButton = document.getElementById("themeToggle");
